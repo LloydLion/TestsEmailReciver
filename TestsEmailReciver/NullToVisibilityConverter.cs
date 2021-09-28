f9a9746220b4fac4229bc003f94ff49058dd8790
LloydLion <55992792+LloydLion@users.noreply.github.com>
2021-09-28 21:43:35 +0300

    Mini ui change

---
+++ TestsEmailReciver/NullToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TestsEmailReciver
+{
+    public sealed class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Visibility)value) == Visibility.Hidden ? null : new object();
+        }
+    }
+}
